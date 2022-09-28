1bdb874fd1bcee700ef94a5d4741dd4508e8b203
shusuke919 <sh091965@gmail.com>
2022-09-28 14:36:58 +0900

    ログイン認証完了

--- app/src/conponents/Navbar.jsx
+++ app/src/conponents/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { auth } from '../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import Signin from './Signin'
+import LogOut from './LogOut'
 const style = {
   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
   heading: `text-white text-3xl`
@@ -13,7 +14,8 @@ const Navbar = () => {
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>Chat App</h1>
-      <Signin />
+      {user ? <LogOut /> : <Signin />}
+    
     </div>
   )
 }
